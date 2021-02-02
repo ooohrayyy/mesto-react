bb2a4f34a717d0c5a16175f7d21e3c44e2baddf0
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-02 18:57:00 +0300

    feat: Настроить закрытие попапов

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -9,7 +9,7 @@ function PopupWithForm (props) {
   return (
     <div className={`popup popup-${props.name} root__popup ${props.isOpen ? 'popup_opened' : null}`}>
         <form className="popup__container" name={props.name} noValidate>
-          <button className="popup__close" type="button"></button>
+          <button className="popup__close" type="button" onClick={props.onClose}></button>
           <h2 className="popup__heading">{props.heading}</h2>
           {props.children}
           {button}
