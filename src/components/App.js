a00a2d66ced9774ad6bc6d5bbb588840114bb77d
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-02 17:32:19 +0300

    feat: Вынести общий компонент попапов с формой

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
+import PopupWithForm from "./PopupWithForm.js";
 
 function App () {
   return (
@@ -10,44 +11,23 @@ function App () {
         <Main />
         <Footer />
       </div>
-      <div className="popup popup-profile root__popup">
-        <form className="popup__container" name="profile" noValidate>
-          <button className="popup__close" type="button"></button>
-          <h2 className="popup__heading">Редактировать профиль</h2>
-          <input className="popup__input popup__input_name" name="name" type="text" placeholder="Имя" minLength="2" maxLength="40" autoComplete="off" required />
-          <span className="popup__error"></span>
-          <input className="popup__input popup__input_description" name="description" type="text" placeholder="Описание" minLength="2" maxLength="200" autoComplete="off" required />
-          <span className="popup__error"></span>
-          <button className="popup__button" type="submit">Сохранить</button>
-        </form>
-      </div>
-      <div className="popup popup-avatar root__popup">
-        <form className="popup__container" name="avatar-form" noValidate>
-          <button className="popup__close" type="button"></button>
-          <h2 className="popup__heading">Обновить аватар</h2>
-          <input className="popup__input popup__input_avatar" name="avatar" type="url" placeholder="Ссылка на фото" autoComplete="off" required />
-          <span className="popup__error"></span>
-          <button className="popup__button" type="submit">Сохранить</button>
-        </form>
-      </div>
-      <div className="popup popup-card root__popup">
-        <form className="popup__container" name="card" noValidate>
-          <button className="popup__close" type="button"></button>
-          <h2 className="popup__heading">Новое место</h2>
-          <input className="popup__input popup__input_card-name" name="place" type="text" placeholder="Название" minLength="2" maxLength="30" autoComplete="off" required />
-          <span className="popup__error"></span>
-          <input className="popup__input popup__input_card-link" name="link" type="url" placeholder="Ссылка на картинку" autoComplete="off" required />
-          <span className="popup__error"></span>
-          <button className="popup__button" type="submit">Создать</button>
-        </form>
-      </div>
-      <div className="popup popup-delete root__popup">
-        <section className="popup__container">
-          <button className="popup__close" type="button"></button>
-          <h2 className="popup__heading popup__heading_delete">Вы уверены?</h2>
-          <button className="popup__button popup__button_type_delete" type="submit">Да</button>
-        </section>
-      </div>
+      <PopupWithForm name="profile" heading="Редактировать профиль">
+        <input className="popup__input popup__input_name" name="name" type="text" placeholder="Имя" minLength="2" maxLength="40" autoComplete="off" required />
+        <span className="popup__error"></span>
+        <input className="popup__input popup__input_description" name="description" type="text" placeholder="Описание" minLength="2" maxLength="200" autoComplete="off" required />
+        <span className="popup__error"></span>
+      </PopupWithForm>
+      <PopupWithForm name="avatar" heading="Обновить аватар">
+        <input className="popup__input popup__input_avatar" name="avatar" type="url" placeholder="Ссылка на фото" autoComplete="off" required />
+        <span className="popup__error"></span>
+      </PopupWithForm>
+      <PopupWithForm name="card" heading="Новое место">
+        <input className="popup__input popup__input_card-name" name="place" type="text" placeholder="Название" minLength="2" maxLength="30" autoComplete="off" required />
+        <span className="popup__error"></span>
+        <input className="popup__input popup__input_card-link" name="link" type="url" placeholder="Ссылка на картинку" autoComplete="off" required />
+        <span className="popup__error"></span>
+      </PopupWithForm>
+      <PopupWithForm name="delete" heading="Вы уверены?" />
       <div className="popup popup-fullpic root__popup">
         <figure className="popup__figure">
           <button className="popup__close popup__close_fullpic" type="button"></button>
---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,21 @@
+function popupWithForm (props) {
+  let button;
+  if (props.name === 'delete') {
+    button = <button className="popup__button" type="submit">Удалить</button>;
+  } else {
+    button = <button className="popup__button" type="submit">Сохранить</button>;
+  }
+  
+  return (
+    <div className={`popup popup-${props.name} root__popup`}>
+        <form className="popup__container" name={props.name} noValidate>
+          <button className="popup__close" type="button"></button>
+          <h2 className="popup__heading">{props.heading}</h2>
+          {props.children}
+          {button}
+        </form>
+    </div>
+  );
+}
+
+export default popupWithForm;
