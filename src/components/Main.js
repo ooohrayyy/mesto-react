18c5d644d83d987b34feadeb738e9f5535f8c34f
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-14 12:40:29 +0300

    feat: Использовать CurrentUserContext в Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,26 +2,15 @@ import React from 'react';
 import api from '../utils/Api.js';
 import Card from './Card.js';
 
-import jaque from '../resources/images/jaque.png';
+// import jaque from '../resources/images/jaque.png';
+import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 
 function Main (props) {
-  const [userName, setUserName] = React.useState('Жак-Ив Кусто');
-  const [userDescription, setUserDescription] = React.useState('Исследователь океана');
-  const [userAvatar, setUserAvatar] = React.useState(jaque);
+  const userInfo = React.useContext(CurrentUserContext);
 
   const [cards, setCards] = React.useState([]);
 
   React.useEffect(() => {
-    api.fetchUserInfo()
-      .then(res => {
-        setUserName(res.name);
-        setUserDescription(res.about);
-        setUserAvatar(res.avatar);
-      })
-      .catch(err => {
-        console.log(`Что-то пошло не так: ${err}`);
-      });
-
     api.fetchInitialCards()
       .then(res => {
         setCards(res);
@@ -32,12 +21,12 @@ function Main (props) {
     <main className="main container__main">
       <section className="profile main__profile">
         <div className="profile__main">
-          <img className="profile__avatar" src={userAvatar} alt="Портрет пользователя" />
+          <img className="profile__avatar" src={userInfo.avatar} alt="Портрет пользователя" />
           <button className="profile__avatar-button" onClick={props.onEditAvatar} />
           <div className="profile__text-info">
-            <h1 className="profile__name">{userName}</h1>
+            <h1 className="profile__name">{userInfo.name}</h1>
             <button className="profile__edit" type="button" onClick={props.onEditProfile} />
-            <p className="profile__description">{userDescription}</p>
+            <p className="profile__description">{userInfo.about}</p>
           </div>
         </div>
         <button className="profile__add" type="button" onClick={props.onAddPlace} />
