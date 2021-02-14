fb3c056b8072a296746968e34cbcf629e2b06221
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-14 14:40:03 +0300

    feat: Добавить управляемые переменные в EditProfilePopup

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,26 +1,50 @@
+import React from 'react';
+
 import PopupWithForm from './PopupWithForm.js';
 
 function EditProfilePopup (props) {
+  // * Управляемые стейт-переменные
+
+  // Имя пользователя
+
+  const [name, setName] = React.useState('');
+
+  function handleNameChange (evt) {
+    setName(evt.target.value);
+  }
+
+  // Описание пользователя
+
+  const [description, setDescription] = React.useState('');
+
+  function handleDescriptionChange (evt) {
+    setDescription(evt.target.value);
+  }
+
   return (
     <PopupWithForm name="profile" heading="Редактировать профиль" isOpen={props.isOpen} onClose={props.onClose}>
       <input
         className="popup__input popup__input_name"
-        name="name"
         type="text"
+        name="name"
+        value={name}
         placeholder="Имя"
         minLength="2" maxLength="40"
         autoComplete="off"
         required
+        onChange={handleNameChange}
       />
       <span className="popup__error"></span>
       <input
         className="popup__input popup__input_description"
-        name="description"
         type="text"
+        name="description"
+        value={description}
         placeholder="Описание"
         minLength="2" maxLength="200"
         autoComplete="off"
         required
+        onChange={handleDescriptionChange}
       />
       <span className="popup__error"></span>
     </PopupWithForm>
