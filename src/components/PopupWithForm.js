2f3e3479b46a446d1a1cf51650d93c182f0bd19f
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-16 18:12:42 +0300

    fix: Ввести изначальное состояние валидности форм

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
 function PopupWithForm (props) {
-  const [formValid, setFormValid] = React.useState(props.initialValidityState); // * Стейт-переменная валидности формы
+  const [formValidity, setFormValidity] = React.useState(props.initialValidityState); // * Стейт-переменная валидности формы
 
-  function handleChange (evt) { // * Хэндлер изменения формы
-    setFormValid(evt.currentTarget.checkValidity());
+  // * Функциии
+
+  function handleChange (evt) { // Хэндлер изменения формы
+    setFormValidity(evt.currentTarget.checkValidity());
+  }
+
+  function handleSubmit (evt) { // Хэндлер сабмита
+    setFormValidity(props.initialValidityState);
+    props.onSubmit(evt);
   }
 
   return (
     <div className={`popup popup-${props.name} root__popup ${props.isOpen ? 'popup_opened' : ''}`}>
-        <form className="popup__container" name={props.name} noValidate onChange={handleChange} onSubmit={props.onSubmit}>
+        <form className="popup__container" name={props.name} noValidate onChange={handleChange} onSubmit={handleSubmit}>
           <button className="popup__close" type="button" onClick={props.onClose} />
           <h2 className="popup__heading">{props.heading}</h2>
           {props.children}
-          <button className="popup__button" type="submit" disabled={!formValid}>
+          <button className="popup__button" type="submit" disabled={!formValidity}>
             {(props.name === 'delete') ? 'Удалить' : 'Сохранить'}
           </button>
         </form>
