c15d82cc69397eaadbdb08845dbf3d6005c61332
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-17 18:03:00 +0300

    improve: Улучшить закрытие попапа с полноразмерной картинкой

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,7 +2,7 @@ function ImagePopup (props) {
   return (
     <div className={`popup popup-fullpic root__popup ${props.isOpen ? 'popup_opened' : ''}`}>
       <figure className="popup__figure">
-        <button className="popup__close popup__close_fullpic" type="button" onClick={props.onClose} />
+        {props.card && (<button className="popup__close popup__close_fullpic" type="button" onClick={props.onClose} />)}
         <img
           className="popup__fullpic"
           src={props.card ? props.card.link : null}
