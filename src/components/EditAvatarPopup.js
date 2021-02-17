c26d1d3c5c09db9cbcbb31a889d77e422a97d8f3
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-17 14:36:07 +0300

    feat: Валидировать форму аватарки с поднятием стейта

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -7,20 +7,45 @@ function EditAvatarPopup (props) {
   // ! Реф инпута с адресом новой аватарки не используется из-за валидации, поскольку передать реф в дочерний компонент вроде как нельзя.
   // ! Зато реф есть в самом Input! :)
 
+  // * Стейт-переменные
+
+  // Ссылка на аватарку
+
   const [link, setLink] = React.useState('');
 
   function handleLinkChange (evt) {
     setLink(evt.target.value);
   }
 
-  function handleSubmit (evt) { // * Хэндлер сабмита
+  const [formValidity, setFormValidity] = React.useState(false); // Состояние валидации
+
+  // * Функции
+
+  function handleSubmit (evt) { // Хэндлер сабмита
     evt.preventDefault();
+
     props.onUpdateAvatar(link);
+
     setLink('');
+    setFormValidity(false);
+  }
+
+  function handleClose () { // Хэндлер закрытия
+    props.onClose();
+
+    setLink('');
+    setFormValidity(false);
   }
 
   return (
-    <PopupWithForm initialValidityState={props.initialValidityState} name="avatar" heading="Обновить аватар" isOpen={props.isOpen} onSubmit={handleSubmit} onClose={props.onClose}>
+    <PopupWithForm
+      validityState={formValidity}
+      onValidityChange={setFormValidity}
+      name="avatar"
+      heading="Обновить аватар"
+      isOpen={props.isOpen}
+      onSubmit={handleSubmit}
+      onClose={handleClose}>
       <Input
         inputModifier="popup__input_avatar"
         inputName="avatar"
