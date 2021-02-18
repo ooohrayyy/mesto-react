23eed1b1623681529ecf6e011f5257901a3a7cd1
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-18 11:32:11 +0300

    refactor: Оптимизировать стили попапов

--- src/components/PopupLoader.js
+++ src/components/PopupLoader.js
@@ -2,9 +2,9 @@ import loader from '../resources/images/svgs/popup-loader.svg';
 
 function PopupLoader (props) {
   return (
-    <div className={`popup__loader popup-loader ${(props.name === 'delete') ? 'popup-loader_delete' : ''}`}>
-      <img className="popup-loader__img" src={loader} alt="Индикатор загрузки" />
-      <h3 className="popup-loader__text">Загрузка...</h3>
+    <div className={`popup__message popup-message ${(props.name === 'delete') ? 'popup-message_delete' : ''}`}>
+      <img className="popup-message__img" src={loader} alt="Индикатор загрузки" />
+      <h3 className="popup-message__text">Загрузка...</h3>
     </div>
   );
 }
