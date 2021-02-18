c44d55c473eccf63a7c7aa8d1e6f05e298a8125c
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-18 11:23:46 +0300

    fix: Вынести внешнюю геометрию с блочных компонентов

--- src/components/PopupError.js
+++ src/components/PopupError.js
@@ -2,7 +2,7 @@ import errorImage from '../resources/images/svgs/popup-error.svg';
 
 function PopupError (props) {
   return (
-    <div className={`popup-error ${(props.name === 'delete') ? 'popup-error_delete' : ''}`}>
+    <div className={`popup__error popup-error ${(props.name === 'delete') ? 'popup-error_delete' : ''}`}>
       <img className="popup-error__img" src={errorImage} alt="Иллюстрация ошибки" />
       <h3 className="popup-error__text">{props.message}</h3>
     </div>
--- src/components/PopupLoader.js
+++ src/components/PopupLoader.js
@@ -2,7 +2,7 @@ import loader from '../resources/images/svgs/popup-loader.svg';
 
 function PopupLoader (props) {
   return (
-    <div className={`popup-loader ${(props.name === 'delete') ? 'popup-loader_delete' : ''}`}>
+    <div className={`popup__loader popup-loader ${(props.name === 'delete') ? 'popup-loader_delete' : ''}`}>
       <img className="popup-loader__img" src={loader} alt="Индикатор загрузки" />
       <h3 className="popup-loader__text">Загрузка...</h3>
     </div>
