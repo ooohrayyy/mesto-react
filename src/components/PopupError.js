273c9a7f9d5da4b5ce489b7534cf418f2065b511
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-18 11:12:41 +0300

    fix: Добавить недостающий alt

--- src/components/PopupError.js
+++ src/components/PopupError.js
@@ -3,7 +3,7 @@ import errorImage from '../resources/images/svgs/popup-error.svg';
 function PopupError (props) {
   return (
     <div className={`popup-error ${(props.name === 'delete') ? 'popup-error_delete' : ''}`}>
-      <img className="popup-error__img" src={errorImage} />
+      <img className="popup-error__img" src={errorImage} alt="Иллюстрация ошибки" />
       <h3 className="popup-error__text">{props.message}</h3>
     </div>
   );
