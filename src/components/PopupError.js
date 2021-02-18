cf03bc487bae1d1e053c2a307955bbdc44cc8207
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-18 10:23:56 +0300

    feat: Создать компонент PopupError

---
+++ src/components/PopupError.js
@@ -0,0 +1,12 @@
+import errorImage from '../resources/images/svgs/popup-error.svg';
+
+function PopupError (props) {
+  return (
+    <div className={`popup-error ${(props.name === 'delete') ? 'popup-error_delete' : ''}`}>
+      <img className="popup-error__img" src={errorImage} />
+      <h3 className="popup-error__text">{props.errorMessage}</h3>
+    </div>
+  );
+}
+
+export default PopupError;
