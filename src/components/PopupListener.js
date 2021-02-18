1b7ff50572d940cb2bb1dc83aff65daf35ddce73
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-18 13:19:06 +0300

    feat: Установить слушатели оверлея на все попапы

--- src/components/PopupListener.js
+++ src/components/PopupListener.js
@@ -2,20 +2,28 @@ import React from 'react';
 
 function PopupListener (props) {
   React.useEffect(() => {
-    function handleEscClose (evt) {
-      if (props.state) {
-        if (evt.key === 'Escape' && !props.state.loading && !props.state.failed) {
-          props.onClose();
-        }
-      } else {
+    function checkState () { // Проверка состояния попапа
+      return (props.state) ? ((!props.state.loading && !props.state.failed) ? true : false) : true;
+    }
+
+    function handleEscClose (evt) { // Хэндлер нажатия на Esc
+      if (evt.key === 'Escape' && checkState()) {
+        props.onClose();
+      }
+    }
+
+    function handleOverlayClose (evt) { // Хэндлер нажатия на оверлей
+      if (evt.target.classList.contains('popup') && checkState()) {
         props.onClose();
       }
     }
 
     document.addEventListener('keydown', handleEscClose);
+    document.addEventListener('click', handleOverlayClose);
 
     return () => {
       document.removeEventListener('keydown', handleEscClose);
+      document.removeEventListener('click', handleOverlayClose);
     }
   }, [props]);
 
