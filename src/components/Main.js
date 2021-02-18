b3c232f2c175a430f2488d193f504f3a1d14f280
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-18 13:32:11 +0300

    feat: Добавить лоадеры для аватарки и профиля

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Card from './Card.js';
 
+import avatarLoader from '../resources/images/svgs/avatar-loader.svg';
+
 import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 
 function Main (props) {
@@ -11,12 +13,12 @@ function Main (props) {
     <main className="main container__main">
       <section className="profile main__profile">
         <div className="profile__main">
-          <img className="profile__avatar" src={userInfo.avatar} alt="Портрет пользователя" />
+          <img className="profile__avatar" src={userInfo.avatar || avatarLoader} alt="Портрет пользователя" />
           <button className="profile__avatar-button" onClick={props.onEditAvatar} />
           <div className="profile__text-info">
-            <h1 className="profile__name">{userInfo.name}</h1>
+            <h1 className="profile__name">{userInfo.name || 'Загрузка...'}</h1>
             <button className="profile__edit" type="button" onClick={props.onEditProfile} />
-            <p className="profile__description">{userInfo.about}</p>
+            <p className="profile__description">{userInfo.about || 'Пожалуйста, подождите'}</p>
           </div>
         </div>
         <button className="profile__add" type="button" onClick={props.onAddPlace} />
