68c1ecdb6bb944d24eed6b09041e2c61ab8f3f52
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-18 11:01:22 +0300

    feat: Добавить возможность перезагрузки при ошибке

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,14 +4,21 @@ import Loader from './Loader.js';
 import PopupError from './PopupError.js';
 
 function PopupWithForm (props) {
-  function handleChange (evt) { // * Хэндлер изменения формы
+  // * Хэндлеры
+
+  function handleChange (evt) { // Хэндлер изменения формы
     props.onFormValidityChange(evt.currentTarget.checkValidity());
+  }
+
+  function handleRefresh () { // Хэндлер кнопки перезагрузки страницы
+    window.location.reload();
   }
 
   return (
     <div className={`popup popup-${props.name} root__popup ${props.isOpen ? 'popup_opened' : ''}`}>
         <form className="popup__container" name={props.name} noValidate onChange={handleChange} onSubmit={props.onSubmit}>
           {(!props.isLoading && !props.didFailed.failed) && (<button className="popup__close" type="button" onClick={props.onClose} />)}
+          {props.didFailed.failed && (<button className="popup__refresh" type="button" onClick={handleRefresh} />)}
           <h2
             className={`popup__heading ${(props.name === 'delete') ? 'popup__heading_delete' : ''}`}
           >
