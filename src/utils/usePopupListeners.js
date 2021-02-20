7afa3683f01a1e0841d35545c60e08ee2da1753b
Boris Nikitashov <ooohrayyy@gmail.com>
2021-02-20 13:41:42 +0300

    chore: Удалить лишний компонент и логи

--- src/utils/usePopupListeners.js
+++ src/utils/usePopupListeners.js
@@ -9,27 +9,23 @@ function usePopupListeners (props) {
   }, [loading, failed]);
 
   const handleEscClose = React.useCallback((evt) => {
-    console.log('Нажата клавиша');
     if ((evt.key === 'Escape') && checkState()) {
       onClose();
     }
   }, [checkState, onClose]);
 
   const handleOverlayClose = React.useCallback((evt) => {
-    console.log('Клик');
     if (evt.target.classList.contains('popup') && checkState()) {
       onClose();
     }
   }, [checkState, onClose]);
 
   const addEventListeners = React.useCallback(() => {
-    console.log('Слушаем!');
     document.addEventListener('keydown', handleEscClose);
     document.addEventListener('click', handleOverlayClose);
   }, [handleEscClose, handleOverlayClose]);
 
   const removeEventListeners = React.useCallback(() => {
-    console.log('Не слушаем');
     document.removeEventListener('keydown', handleEscClose);
     document.removeEventListener('click', handleOverlayClose);
   }, [handleEscClose, handleOverlayClose]);
